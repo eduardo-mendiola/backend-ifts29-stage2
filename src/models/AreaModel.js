2557157b6669edf3ee45f9a234df9a3d760622dd
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2025-09-27 22:04:36 -0300

    fix: AreaModel class

--- src/models/AreaModel.js
+++ src/models/AreaModel.js
@@ -1,9 +1,26 @@
 import mongoose from 'mongoose';
+import BaseModel from './BaseModel.js';
 
 const areaSchema = new mongoose.Schema({
     name_area: { type: String, required: true }
-}, { 
-    collection: 'areas'
-});
+}, { collection: 'areas' });
 
-export default mongoose.models.Area || mongoose.model('Area', areaSchema);
+class AreaModel extends BaseModel {
+    constructor() {
+        super(areaSchema, 'Area');
+    }
+
+    // more area-specific methods can be added here
+}
+
+export default new AreaModel();
+
+// import mongoose from 'mongoose';
+
+// const areaSchema = new mongoose.Schema({
+//     name_area: { type: String, required: true }
+// }, { 
+//     collection: 'areas'
+// });
+
+// export default mongoose.models.Area || mongoose.model('Area', areaSchema);
