33e697bcf088f2c0ffddb628b8a61551d7399fad
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2025-09-29 19:16:42 -0300

    refactor: remove unused entity classes

--- src/entities/AreaEntity.js
+++
@@ -1,9 +0,0 @@
-class AreaEntity {
-    constructor(id, name_area, description) {
-        this.id = id;
-        this.name_area = name_area;
-        this.description = description;
-    }
-}
-
-export default AreaEntity;
